eaf41989e12ca4b30dd6308892b36885442502f2
Your Mom <yourmom@example.com>
2026-05-01 18:38:04 +0300

    Implement cache with automatic cleanup of old entries

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -11,15 +11,20 @@ type cacheEntry struct {
 }
 
 type Cache struct {
-	entries map[string]cacheEntry
 	mu      sync.Mutex
+	entries map[string]cacheEntry
 }
 
-func NewCache(cleanupInterval time.Duration) Cache {
-	return Cache{make(map[string]cacheEntry), sync.Mutex{}}
+func NewCache(cleanupInterval time.Duration) *Cache {
+	c := Cache{sync.Mutex{}, make(map[string]cacheEntry)}
+	go c.reapLoop(cleanupInterval)
+	return &c
 }
 
 func (c *Cache) Add(link string, data []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
 	c.entries[link] = cacheEntry{
 		createdAt: time.Now(),
 		data:      data,
@@ -27,6 +32,25 @@ func (c *Cache) Add(link string, data []byte) {
 }
 
 func (c *Cache) Get(link string) ([]byte, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
 	entry, ok := c.entries[link]
 	return entry.data, ok
+}
+
+func (c *Cache) reapLoop(ticks time.Duration) {
+	ticker := time.NewTicker(ticks)
+	defer ticker.Stop()
+
+	for t := range ticker.C {
+		deadline := t.Add(-ticks)
+		c.mu.Lock()
+		for link, entry := range c.entries {
+			if entry.createdAt.Before(deadline) {
+				delete(c.entries, link)
+			}
+		}
+		c.mu.Unlock()
+	}
 }
